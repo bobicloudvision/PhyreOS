bbeadbd4244c239374876cf0171d8857dc2f311d
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2025-03-03 17:03:25 +0200

    update

---
+++ buildroot/build.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y build-essential bison flex gettext ncurses-dev texinfo unzip wget cpio rsync python3
+
+git clone https://git.buildroot.net/buildroot
+cd buildroot
+
+make menuconfig
