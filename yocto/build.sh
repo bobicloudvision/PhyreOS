695381cf9102c9e6c5a6b991540c0a18ad392353
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2025-03-03 17:40:06 +0200

    update

--- yocto/build.sh
+++ yocto/build.sh
@@ -1,17 +1,14 @@
-sudo apt update
-sudo apt install -y gawk wget git diffstat unzip \
-    texinfo gcc-multilib build-essential chrpath socat \
-    cpio python3 python3-pip python3-pexpect xz-utils \
-    debianutils iputils-ping python3-git python3-jinja2 \
-    libegl1-mesa libsdl1.2-dev xterm
+#sudo apt update
+#sudo apt install -y gawk wget git diffstat unzip \
+#    texinfo gcc-multilib build-essential chrpath socat \
+#    cpio python3 python3-pip python3-pexpect xz-utils \
+#    debianutils iputils-ping python3-git python3-jinja2 \
+#    libegl1-mesa libsdl1.2-dev xterm
+#
 
-
-git clone -b kirkstone git://git.yoctoproject.org/poky.git
+#git clone -b kirkstone git://git.yoctoproject.org/poky.git
 cd poky
 
-source oe-init-build-env
+#source oe-init-build-env
 
-# Add the following to conf/local.conf:
-echo "bitbake core-image-sato" >> conf/local.conf
-bitbake core-image-sato
-IMAGE_INSTALL:append = " coreutils"
+#bitbake core-image-sato
